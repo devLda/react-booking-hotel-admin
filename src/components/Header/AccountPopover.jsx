a98d310a108236684ee030821f9f5d2cdefa3d45
devLda <leducanh.mywork@gmail.com>
2023-04-27 23:42:58 +0700

    update 27/04

--- src/components/Header/AccountPopover.jsx
+++ src/components/Header/AccountPopover.jsx
@@ -1,112 +1,122 @@
-import { useState } from 'react';
+import { useState } from "react";
 // @mui
-import { alpha } from '@mui/material/styles';
-import { Box, Divider, Typography, Stack, MenuItem, Avatar, IconButton, Popover } from '@mui/material';
+import { alpha } from "@mui/material/styles";
+import {
+  Box,
+  Divider,
+  Typography,
+  Stack,
+  MenuItem,
+  Avatar,
+  IconButton,
+  Popover,
+} from "@mui/material";
+import avata from "../../assets/images/avatars/avatar_default.jpg";
 // mocks_
 const account = {
-    displayName: 'Lê Đức Anh',
-    email: 'leducanh@gmail.com',
-    photoURL: '../../assets/images/avatars/avatar_default.jpg',
-  };
+  displayName: "Lê Đức Anh",
+  email: "leducanh@gmail.com",
+  photoURL: avata,
+};
 
 // ----------------------------------------------------------------------
 
 const MENU_OPTIONS = [
   {
-    label: 'Home',
-    icon: 'eva:home-fill',
+    label: "Home",
+    icon: "eva:home-fill",
   },
   {
-    label: 'Profile',
-    icon: 'eva:person-fill',
+    label: "Profile",
+    icon: "eva:person-fill",
   },
   {
-    label: 'Settings',
-    icon: 'eva:settings-2-fill',
+    label: "Settings",
+    icon: "eva:settings-2-fill",
   },
 ];
 
 // ----------------------------------------------------------------------
 
 const AccountPopover = () => {
-    const [open, setOpen] = useState(null);
+  const [open, setOpen] = useState(null);
 
-    const handleOpen = (event) => {
-      setOpen(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setOpen(null);
-    };
-  
-    return (
-      <>
-        <IconButton
-          onClick={handleOpen}
-          sx={{
-            p: 0,
-            ...(open && {
-              '&:before': {
-                zIndex: 1,
-                content: "''",
-                width: '100%',
-                height: '100%',
-                borderRadius: '50%',
-                position: 'absolute',
-                bgcolor: (theme) => alpha(theme.palette.grey[900], 0.8),
-              },
-            }),
-          }}
-        >
-          <Avatar src={account.photoURL} alt="photoURL" />
-        </IconButton>
-  
-        <Popover
-          open={Boolean(open)}
-          anchorEl={open}
-          onClose={handleClose}
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-          PaperProps={{
-            sx: {
-              p: 0,
-              mt: 1.5,
-              ml: 0.75,
-              width: 180,
-              '& .MuiMenuItem-root': {
-                typography: 'body2',
-                borderRadius: 0.75,
-              },
+  const handleOpen = (event) => {
+    setOpen(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+
+  return (
+    <>
+      <IconButton
+        onClick={handleOpen}
+        sx={{
+          p: 0,
+          ...(open && {
+            "&:before": {
+              zIndex: 1,
+              content: "''",
+              width: "100%",
+              height: "100%",
+              borderRadius: "50%",
+              position: "absolute",
+              bgcolor: (theme) => alpha(theme.palette.grey[900], 0.8),
             },
-          }}
-        >
-          <Box sx={{ my: 1.5, px: 2.5 }}>
-            <Typography variant="subtitle2" noWrap>
-              {account.displayName}
-            </Typography>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
-              {account.email}
-            </Typography>
-          </Box>
-  
-          <Divider sx={{ borderStyle: 'dashed' }} />
-  
-          <Stack sx={{ p: 1 }}>
-            {MENU_OPTIONS.map((option) => (
-              <MenuItem key={option.label} onClick={handleClose}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </Stack>
-  
-          <Divider sx={{ borderStyle: 'dashed' }} />
-  
-          <MenuItem onClick={handleClose} sx={{ m: 1 }}>
-            Logout
-          </MenuItem>
-        </Popover>
-      </>
-    );
-}
+          }),
+        }}
+      >
+        <Avatar src={account.photoURL} alt="photoURL" />
+      </IconButton>
 
-export default AccountPopover
+      <Popover
+        open={Boolean(open)}
+        anchorEl={open}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        PaperProps={{
+          sx: {
+            p: 0,
+            mt: 1.5,
+            ml: 0.75,
+            width: 180,
+            "& .MuiMenuItem-root": {
+              typography: "body2",
+              borderRadius: 0.75,
+            },
+          },
+        }}
+      >
+        <Box sx={{ my: 1.5, px: 2.5 }}>
+          <Typography variant="subtitle2" noWrap>
+            {account.displayName}
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }} noWrap>
+            {account.email}
+          </Typography>
+        </Box>
+
+        <Divider sx={{ borderStyle: "dashed" }} />
+
+        <Stack sx={{ p: 1 }}>
+          {MENU_OPTIONS.map((option) => (
+            <MenuItem key={option.label} onClick={handleClose}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Stack>
+
+        <Divider sx={{ borderStyle: "dashed" }} />
+
+        <MenuItem onClick={handleClose} sx={{ m: 1 }}>
+          Logout
+        </MenuItem>
+      </Popover>
+    </>
+  );
+};
+
+export default AccountPopover;
